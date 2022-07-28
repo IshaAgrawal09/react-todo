b2d13394fab0957be12a46dbe393029cf5a42df0
IshaAgrawal09 <ishaagrawal@cedcoss.com>
2022-07-28 18:41:13 +0530

    TODO

--- src/App.js
+++ src/App.js
@@ -1,44 +1,18 @@
-import React,{ useState } from 'react';
-
-import Add from './Add';
-import './App.css';
-import Complete from './Complete';
-import Incomplete from './Incomplete';
-
+import React, { useState } from "react";
+import "./App.css";
+import Todo from "./Todo";
 function App() {
-
-  const [text,setText] = useState("");
-  const [count,setCount] =  useState(1);
-  const [itemarr,setItemarr] = useState([]);
-  const [comp,setComp] = useState([]);
+  const [todoList, setTodoList] = useState([]);
+  const [count, setCount] = useState(1);
 
   return (
-    <div className="App">
-      <div className='todo'>
-        <Add 
-        text = {text}
-        setText = {setText}
-        count = {count}
-        setCount = {setCount}
-        itemarr = {itemarr}
-        setItemarr = {setItemarr}
-        />
-        <Incomplete 
-          setText = {setText}
-          textArr = {itemarr}
-          setItemarr = {setItemarr}
-          comp = {comp}
-          setComp = {setComp}
-        />
-        <Complete 
-          comp = {comp}
-          setComp = {setComp}
-          textArr = {itemarr}
-          setItemarr = {setItemarr}
-          count = {count}
-          setCount = {setCount}
-        />
-      </div>
+    <div className="todo">
+      <Todo
+        todoList={todoList}
+        setTodoList={setTodoList}
+        count={count}
+        setCount={setCount}
+      />
     </div>
   );
 }
